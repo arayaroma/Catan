4f914da9ac21c13d5425710c1c6b4d143ecbb534
Dilan <dio3sancho@gmail.com>
2023-01-03 20:53:15 -0600

    We added new headers of load and action to separate the program header

--- Proyecto_Catan_VSM/Program.h
+++ Proyecto_Catan_VSM/Program.h
@@ -12,21 +12,10 @@ private:
 	Stack* cards = new Stack();
 public:
 	Program();
-	void assemblyLand();
-	void assignLand();
-	void firstMove(std::string);
-	void placeRoad();
-	void placeTown();
 	void play();
-	void assignMaterial();
-	void createCardStacks();
-	void loadCards();
-	void loadMaterialCards();
-	void loadDevelopCards();
-	void shuffleCard();
-	int rollDice();
-	void tradeMaterials();
 	void build();
+	void tradeMaterials();
+	void assemblyLand();
 	void playDevelopCard();
 };
 
