a5ae70ab2bb827f34163eb3acb07f39889b36943
arayaroma <darayaroma@gmail.com>
2023-01-07 17:06:33 -0600

    Se limpió el proyecto, ahora muestra Sources Files y Headers Files

---
+++ Catan/main.cpp
@@ -0,0 +1,20 @@
+#include "CImg.h"
+#include "Label.hpp"
+#include "Program.hpp"
+#include "Window.hpp"
+#include <iostream>
+#include <string>
+
+using namespace cimg_library;
+using namespace std;
+
+int main() {
+	Program program;
+	program.assemblyLand();
+
+	Window window;
+	window.printTitleScreenLabels();
+	window.showDisplay();
+
+	return 0;
+}
