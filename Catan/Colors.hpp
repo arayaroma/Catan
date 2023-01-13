55f3ffb5f50b3964f9d571e77b04396f6ba9e054
arayaroma <darayaroma@gmail.com>
2023-01-13 00:44:17 -0600

    Colors Header File
    Para no reescribir codigo, agregué este Header, y porque se vuelve util al usarlo en distintas clases.

---
+++ Catan/Colors.hpp
@@ -0,0 +1,11 @@
+#pragma once
+const int transparent = 0;
+
+const unsigned char BLACK[] = {0, 0, 0};
+const unsigned char WHITE[] = {255, 255, 255};
+
+const unsigned char DARKGRAY[] = {35, 35, 35};
+const unsigned char GRAY[] = {55, 55, 55};
+
+const unsigned char BLUEGREEN[] = {0, 170, 255};
+const unsigned char ORANGE[] = {255, 140, 0};
