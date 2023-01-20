45a0ee5655582db1b20e5146f45aba5c342bd7ae
arayaroma <darayaroma@gmail.com>
2023-01-20 15:49:17 -0600

    Match Eliminado

--- Catan/Match.cpp
+++
@@ -1,9 +0,0 @@
-#include "Match.hpp"
-
-Match::Match() {}
-
-void Match::setName(const char *name) { this->name = name; }
-const char *Match::getName() const { return this->name; }
-
-void Match::setMatchID(int matchID) { this->matchID = matchID; }
-int Match::getMatchID() const { return this->matchID; }
