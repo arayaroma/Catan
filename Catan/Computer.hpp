89316184bc44f7a1aa5773004e27a366b3f4e421
Dilan <dio3sancho@gmail.com>
2023-01-20 23:31:45 -0600

    Modificacion de clases, implementacion de clase human y computer con atributos, sets y gets

--- Catan/Computer.hpp
+++ Catan/Computer.hpp
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
+using namespace std; 
+
 class Computer
 {
+public:
+
+	Computer();
+
+	void setName(string);
+	string getName();
+
+	void setColor(string);
+	string getColor();
+
+	void play();
+
+private:
+
+	string name;
+	string color;
+	int numTurn;
 };
 
---
+++ Catan/Human.cpp
@@ -0,0 +1,13 @@
+#include "Human.h"
+
+Human::Human(){}
+
+void Human::setName(string name){name = name; }
+string Human::getName() {return name;}
+void Human::setColor(string color){color = color;}
+string Human::getColor(){return color; }
+
+void Human::play() 
+{
+
+}
